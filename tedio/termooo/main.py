33a5206c2e2eb704ca466ee766d69fb0c4cd987b
Ian Jabriel <97356482+IanJabriel@users.noreply.github.com>
2025-07-24 14:39:44 -0300

    Add files via upload
    add requirement e match-case na main.py

--- tedio/termooo/main.py
+++ tedio/termooo/main.py
@@ -5,21 +5,22 @@ from dueto import TermoooDueto
 def main():
     print("Bem-vindo ao Termooo!")
     print("1 - Termooo\n2 - Dueto")
-    
+
     try:
         opcao = int(input("Escolha o modo (1 ou 2): "))
     except ValueError:
         print("Erro: valor inválido. Esperado um número inteiro (1 ou 2).")
         return
-
-    if opcao == 1:
-        jogo = TermoooClassico()
-        jogo.jogar()
-    elif opcao == 2:
-        jogo = TermoooDueto()
-        jogo.jogar()
-    else:
-        print("Modo inválido.")
+    
+    match opcao:
+        case 1:
+            jogo = TermoooClassico()
+            jogo.jogar()
+        case 2:
+            jogo = TermoooDueto()
+            jogo.jogar()
+        case _:
+            print("Modo inválido.")
 
 if __name__ == "__main__":
     main()
