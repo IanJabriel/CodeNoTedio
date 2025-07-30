860672dc78539b6147fbed4eb08a1548467dba18
Ian Jabriel <97356482+IanJabriel@users.noreply.github.com>
2025-07-30 12:36:06 -0300

    Delete termooo directory

--- termooo/test.py
+++
@@ -1,8 +0,0 @@
-import unicodedata
-
-a = input("Digite sua palavra: ")
-
-def remove_acento(a):
-    return ''.join(x for x in unicodedata.normalize("NFD",a)if unicodedata.category(x) != 'Mn')
-
-print(remove_acento(a))
